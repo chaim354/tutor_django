1e32ffeb0a9d1ea1b4f6369edbf8b1952464fb28
Charles Schwartz <chaim354@gmail.com>
2025-01-14 13:41:04 -0500

    flowfix

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -55,6 +55,6 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
-        require('flowbite')
+        require('flowbite/plugin')
     ],
 }
